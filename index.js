4d72d259e741879331a8a217e6c672c1da8eee34
Iankamau1 <theurikamau7@gmail.com>
2023-07-04 23:31:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add(1, 80));
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subtract(60-40))
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(multiply(2 * 3.4))
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(divide(5.0 / 2.5))
+
+function increment(number) {
+    return number + 1;
+}
+
+console.log(increment( number + 1))
+
+function decrement(number) {
+    return number - 1;
+}
+
+console.log(increment( number - 1))
+
+function makeInt(number) {
+    return parseInt (number, 10);
+}
+
+console.log(makeInt("7", 10))
+
+function preserveDecimal(number) {
+    return parseFloat (number, 10);
+}
+
+console.log(preserveDecimal("7", 10))
